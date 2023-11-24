a79dcd51c5e2103cb99a8e44a1390501ae06c747
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-24 10:42:57 +0000

    grabbed elements off the DOM and added event listeners to the video

---
+++ script.js
@@ -0,0 +1,12 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Event listeners
+
+video.addEventListener("click", (toggleVideoStatus = () => {}));
+video.addEventListener("pause", (updatePlayIcon = () => {}));
+video.addEventListener("play", (updatePlayIcon = () => {}));
+video.addEventListener("timeupdate", (updateProgress = () => {}));
